8de9f737ff1879a9df9d3e216bedfdf933571f04
Ricardo Silva <ricardolopes.jazz@gmail.com>
2025-09-16 16:54:46 +0200

    feature: novo bucket

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "aws_s3_bucket" "balde-de-lixo" {
   bucket = var.nome_do_balde
   tags   = var.tags
 }
+
+resource "aws_s3_bucket" "balde-de-lixo2" {
+  bucket = "balde-de-lixo-do-plancton2"
+  tags   = var.tags
+}
