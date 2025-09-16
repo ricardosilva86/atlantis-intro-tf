45d2109ed2b071b0668f96055a3e937a0428a204
Ricardo Silva <ricardolopes.jazz@gmail.com>
2025-09-16 20:09:11 +0000

    feature: criando um novo balde

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "aws_s3_bucket" "balde-de-lixo" {
   bucket = var.nome_do_balde
   tags   = var.tags
 }
+
+resource "aws_s3_bucket" "balde-de-lixo2" {
+  bucket = "${var.nome_do_balde}-2"
+  tags   = var.tags
+}
