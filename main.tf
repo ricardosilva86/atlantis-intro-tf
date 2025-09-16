a6cc847be1f3e4b58638a2cb1b61aa29202ab651
Ricardo Silva <ricardolopes.jazz@gmail.com>
2025-09-16 16:36:14 +0000

    feature: removendo um bucket

--- main.tf
+++ main.tf
@@ -2,8 +2,3 @@ resource "aws_s3_bucket" "balde-de-lixo" {
   bucket = var.nome_do_balde
   tags   = var.tags
 }
-
-resource "aws_s3_bucket" "balde-de-lixo2" {
-  bucket = "balde-de-lixo-do-plancton2"
-  tags   = var.tags
-}
